b08605cf70013df11c5e86cab990d55bc6d59d64
Ben Walley <benwalleyorigami@gmail.com>
2017-07-08 23:58:15 -0500

    styling nav bar, and added search bar, which doesnt work

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,17 +2,60 @@ var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
 
-var Nav = () => {
-	return(
-			<div>
+var Nav = React.createClass({
+
+	onSearch: function(e){
+		e.preventDefault();
+		alert("This Doesn't WORK!!!")
+	},
+
+	render: function(){
+		return(
+			<div className = "top-bar">
+				<div className = "top-bar-left">
+					<ul className = "menu">
+						<li className = "menu-text">
+							The Weather Yo!
+						</li>
+						<li>
+							<IndexLink to = "/" activeClassName = "active" activeStyle = {{fontWeight: 'bold'}}>Get Weather</IndexLink>
+						</li>
+						<li>
+							<Link to = "/about" activeClassName = "active" activeStyle = {{fontWeight: 'bold'}}>About</Link>
+						</li>
+						<li>
+							<Link to = "/examples" activeClassName = "active" activeStyle = {{fontWeight: 'bold'}}>Examples</Link>
+						</li>
+					</ul>
+				</div>
+				<div className = "top-bar-right">
+					<form onSubmit={this.onSearch}>
+						<ul className = "menu">
+							<li>
+								<input type = "search" placeholder='Search Weather'/>
+							</li>
+							<li>
+								<input type = "submit" className = "button" value = "Get Weather"/>
+							</li>
+						</ul>
+					</form>
+				</div>
+
+			</div>
+				
+		);
+	}
+}); 
+
+module.exports = Nav
+
+
+var old = (
+	<div>
 				<h2>Navbar</h2>
 				<IndexLink to = "/" activeClassName = "active" activeStyle = {{fontWeight: 'bold'}}>Get Weather</IndexLink>
 				<Link to = "/about" activeClassName = "active" activeStyle = {{fontWeight: 'bold'}}>About</Link>
 				<Link to = "/examples" activeClassName = "active" activeStyle = {{fontWeight: 'bold'}}>Examples</Link>
 
 			</div>
-			
-		);
-}
-
-module.exports = Nav
+			)
